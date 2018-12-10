7d67f2e07b25485c125a9fe9bf4927ab1b1f1f8a
carissa morrow <carissamorrow@hotmail.com>
2018-12-09 17:25:49 -0700

    so close!

--- src/components/gameEnd.vue
+++ src/components/gameEnd.vue
@@ -1,12 +1,21 @@
 <template>
   <div class="gameEnd">
-    <div v-if="this.game.over == true && this.game.winner != 'Cats Game'">
-      <h1>{{this.game.winner.name}} Wins!!</h1>
+    <div v-if="game.opponent.dead && game.player.dead" class="gameEnd">
+      <h3>Tie!</h3>
+    </div>
+    <div class="gameEnd" v-if="!game.opponent.dead && game.player.dead">
+      <h3>Just Not Your Day</h3>
+    </div>
+    <div class="gameEnd" v-if="game.opponent.dead && !game.player.dead">
+      <h2>You Win</h2>
     </div>
   </div>
 </template>
 
 <script>
+  import player from "@/components/player.vue"
+  import opponent from "@/components/opponent.vue"
+
   export default {
     name: 'gameEnd',
     data() {
@@ -22,6 +31,10 @@
       player() {
         return this.$store.state.player
       }
+    },
+    components: {
+      opponent,
+      player
     },
     methods: {
       gameEnd() {
